7b73d1768b6d99a4d8cc749e307285f88f31f455
jaehoon-lim-0000 <78387047+jaehoon-lim-0000@users.noreply.github.com>
2021-11-19 03:23:41 -0500

    Update UserPool.js

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData= {
-    UserPoolId: "us-east-1_zs3j03Pdf",
-    ClientId: "490mdd65gbkc4p6eaeap0kpsfv"
+    UserPoolId: "",
+    ClientId: ""
 }
 
 export default new CognitoUserPool(poolData);
